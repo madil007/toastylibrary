783576151dc89f187d3817b3942f3ebb4423f7c0
DESKTOP-7MS7461\M.Adil <muhammad.adil@devbatch.com>
2023-06-20 17:48:15 +0500

    toasty library

--- app/src/main/java/com/hostel/toastylibrary/MainActivity.kt
+++ app/src/main/java/com/hostel/toastylibrary/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
          Toaster.showToast(this, "My Toast")
+
     }
 }
